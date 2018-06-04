1cc9d0c164228535ad1ea9c78461b50915067188
Rudra Jikadra <rhjikadra@gmail.com>
2018-06-05 01:31:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FlappyBirdClone-Corona-SDK
---
+++ main.lua
@@ -0,0 +1,20 @@
+display.setStatusBar( display.HiddenStatusBar )
+
+
+local storyboard = require "storyboard"
+
+local bg_sound = audio.loadStream("bgSound.mp3")
+
+local rj1 = display.newImageRect("rj1.png", 1800, 2700)
+rj1.anchorX = 0.5
+rj1.anchorY = 0.5
+rj1.x = display.contentCenterX
+rj1.y = display.contentCenterY
+transition.fadeOut( rj1, { time=1000, delay=5000 } )
+
+
+audio.play(bg_sound, {loops=-1})
+
+
+storyboard.gotoScene( "start" )
+
